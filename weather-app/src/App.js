99170e45579bfbb5d18e8e6d38cabb1fa18d3059
Benson <36316652+Jinb2@users.noreply.github.com>
2021-06-24 21:05:39 -0400

    Add lifecycle for fetching weather
    fetchWeatherJSON is called every minute to update the temperature and description

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,7 +1,9 @@
-import './styling/App.css';
-import React, {useState} from 'react';
+import './styling/app.css';
+import React, {useState, useEffect} from 'react';
 import Days from './components/Days';
 import Navbar from './components/Navbar';
+import Clock from './components/Clock';
+
 
 function App() {
 
@@ -21,36 +23,51 @@ function App() {
       });
     }};
 
-    // get location
-    getLocation();
+  // get location
+  getLocation();
   
-   // temp and desc
-   const [temp, setTemp] = useState(null);
-   const [desc, setDesc] = useState(null);
+  // temp and desc
+  const [temp, setTemp] = useState(null);
+  const [desc, setDesc] = useState(null);
  
-   const api_key = "dc857f068b2ea1e49b1f822db8641377"
+  const api_key = "dc857f068b2ea1e49b1f822db8641377"
  
-   // fetch data from this url
-   async function fetchWeatherJSON(){
-       // wait for promise
-       const response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lng+"&units=imperial&appid="+api_key);
-       const json = await response.json();
- 
-       // set the state for our temp and desc
-       setTemp(json['main']['temp']);
-       setDesc(json['weather'][0]['description']);
-   } 
+  // fetch data from this url
+  async function fetchWeatherJSON(){
+      // wait for promise
+      const response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lng+"&units=imperial&appid="+api_key);
+      const json = await response.json();
+  
+      // set the state for our temp and desc
+      setTemp(json['main']['temp']);
+      setDesc(json['weather'][0]['description']);
+    }   
 
-    // call fetch weather if the lat and lng has been set
+
+   // every minute update the weather
+  useEffect(() => {
+
+    // call function so it appears instead of delaying by the one minute
     if(lat && lng != null){
       fetchWeatherJSON();
     }
+
+    // call function after every minute
+    const interval = setInterval(() => {
+    if(lat && lng != null){
+    fetchWeatherJSON();
+    }
+    },6000);
+    
+    return () => clearInterval(interval);
+    });
+  
 
   return (
     <div className="App">
       <Navbar />
       <Days lng = {lng} lat={lat} temp={temp} desc={desc} />
-
+      <Clock/>
     </div>
   );
 }
---
+++ weather-app/src/components/Clock.js
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from 'react';
+
+
+const Clock = () => {
+
+    const [time, setTime] = useState(null);
+
+    return(
+        <h2>
+            Time
+        </h2>
+
+    )
+}
+
+export default Clock;
